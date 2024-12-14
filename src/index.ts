e7135be767d528f0b792726f0bd40e3d1194739c
Erric Rapsing <erric.rapsing@gmail.com>
2024-12-15 01:15:51 +0800

    CHORE: remove duplicate export statement

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./implementations/db-in-memory";
-export * from "./implementations/db-in-memory";
 export * from "./implementations/queue";
 export * from "./types/dto";
 export * from "./types/enums";
